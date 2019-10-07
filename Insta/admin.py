3a01eb278fd1f3d92a37604722c17b700a07211d
Cinyee <cinyee@Xinyi-MacBook-Pro.local>
2019-10-06 21:22:22 -0400

    Django model

--- Insta/admin.py
+++ Insta/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from Insta.models import Post
 # Register your models here.
+admin.site.register(Post)
--- InstaXY/settings.py
+++ InstaXY/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Insta'
+    'Insta',
+    'imagekit',
 ]
 
 MIDDLEWARE = [
